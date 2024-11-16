2d8360a317d17c1c4a190cbbb2b74f5ffdc27ce0
lubro0 <142106680+lubro0@users.noreply.github.com>
2024-11-16 14:39:47 +0100

    Update Main.php

--- src/Main.php
+++ src/Main.php
@@ -4,6 +4,12 @@ namespace lubro\ServerManager;
 
 use pocketmine\plugin\PluginBase;
 
-class Main extends PluginBase{
+class Main extends PluginBase {
 
+    public function onEnable(): void {
+        if ($this->getServer()->getPluginManager()->getPlugin("FormAPI") !== null) {
+            return;
+        }
+        $this->getServer()->getPluginManager()->disablePlugin($this);
+    }
 }
