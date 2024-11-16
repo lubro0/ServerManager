06be4f48a80fa87ea9c848dc8b7c4d770c5217e7
lubro0 <142106680+lubro0@users.noreply.github.com>
2024-11-16 18:49:36 +0100

    Update Main.php

--- src/Main.php
+++ src/Main.php
@@ -3,7 +3,40 @@
 namespace lubro\ServerManager;
 
 use pocketmine\plugin\PluginBase;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\player\Player;
+use jojoe77777\FormAPI\SimpleForm;
 
 class Main extends PluginBase {
 
+    public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool {
+        if ($command->getName() === "servermanager") {
+            if (!$sender instanceof Player) {
+                $sender->sendMessage("This command can only be used in-game.");
+                return true;
+            }
+            if (!$sender->hasPermission("servermanager.open")) {
+                $sender->sendMessage("You do not have permission to use this command.");
+                return true;
+            }
+            $this->sendServerManagerForm($sender);
+            return true;
+        }
+        return false;
+    }
+
+    private function sendServerManagerForm(Player $player): void {
+        $form = new SimpleForm(function (Player $player, ?int $data) {
+            // Handle form response here if needed
+        });
+
+        $os = PHP_OS;
+
+        $form->setTitle("Server Manager");
+        $form->setContent("Operating System: " . $os);
+        $form->addButton("Close");
+
+        $player->sendForm($form);
+    }
 }
