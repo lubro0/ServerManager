a478d89cc47537549eb1c19a3b4a8e3777571107
lubro0 <142106680+lubro0@users.noreply.github.com>
2024-11-17 18:23:13 +0100

    typo

--- src/Main.php
+++ src/Main.php
@@ -6,7 +6,7 @@ use pocketmine\plugin\PluginBase;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\player\Player;
-use jojoe77777\FormAPI\SimpleForm;
+use jojoe77777\FormAPI\Form;
 
 class Main extends PluginBase {
 
@@ -27,12 +27,32 @@ class Main extends PluginBase {
     }
 
     private function sendServerManagerForm(Player $player): void {
-        $form = new SimpleForm(function (Player $player, $data) {
+        $form = new class() extends Form {
+            private $callback;
+
+            public function __construct(callable $callback) {
+                $this->callback = $callback;
+            }
+
+            public function handleResponse(Player $player, $data): void {
+                ($this->callback)($player, $data);
+            }
+
+            public function jsonSerialize(): array {
+                $os = PHP_OS;
+                return [
+                    "type" => "form",
+                    "title" => "Server Manager",
+                    "content" => "Operating System: " . $os,
+                    "buttons" => [
+                        ["text" => "Close"]
+                    ]
+                ];
+            }
+        };
+
+        $player->sendForm(new $form(function (Player $player, $data) {
             // Handle form response
-        });
-        $form->setTitle("Server Manager");
-        $form->setContent("Operating System: " . PHP_OS);
-        $form->addButton("Close");
-        $player->sendForm($form);
+        }));
     }
 }
